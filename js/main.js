c5370b8b33e086013989473a2e468ee68e64a377
Rafael Caixeta <caixetadev@gmail.com>
2021-07-31 17:18:56 -0300

    resolve

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@ const DOM = {
       <td class="${CSSclass}">${amount}</td>
       <td class="date">${transaction.date}</td>
       <td>
-        <img onclick ="Transaction.remove(${index})"src="/assets/minus.svg" alt="Remover Transação">
+        <img onclick ="Transaction.remove(${index})"src="./assets/minus.svg" alt="Remover Transação">
       </td>
     `
     return html
